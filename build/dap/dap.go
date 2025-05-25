1dabbcf696f55d90795f474c97da29e3e654bbf6
Jonathan A. Sternberg <jonathan@sternberg.sh>
2025-05-24 22:01:44 -0500

    perform solve requests in the context of the dap server

--- build/dap/dap.go
+++ build/dap/dap.go
@@ -71,45 +71,52 @@ func New(rdwr io.ReadWriter) build.Handler {
 }
 
 type debugAdapter struct {
-	initialized chan struct{}
-	started     chan struct{}
+	initialized   chan struct{}
+	started       chan struct{}
+	configuration chan struct{}
 
 	paused   chan struct{}
 	pausedMu sync.Mutex
+
+	solveReqCh chan *solveRequest
 }
 
 func newDebugAdapter() *debugAdapter {
 	return &debugAdapter{
-		initialized: make(chan struct{}),
-		started:     make(chan struct{}),
+		initialized:   make(chan struct{}),
+		started:       make(chan struct{}),
+		configuration: make(chan struct{}),
+		solveReqCh:    make(chan *solveRequest),
 	}
 }
 
 func (d *debugAdapter) Initialize(c Context, req *dap.InitializeRequest, resp *dap.InitializeResponse) error {
 	close(d.initialized)
 
-	c.Go(func(c2 Context) {
-		// Wait for the initialize request to return and then send initialized
-		// event immediately.
-		<-c.Done()
-
-		// Send initialized event.
-		c2.C() <- &dap.InitializedEvent{
-			Event: dap.Event{
-				Event: "initialized",
-			},
-		}
-	})
+	// Set capabilities.
+	resp.Body.SupportsConfigurationDoneRequest = true
 	return nil
 }
 
 func (d *debugAdapter) Launch(c Context, req *dap.LaunchRequest, resp *dap.LaunchResponse) error {
 	close(d.started)
+
+	// Send initialized event to tell the debug adapter
+	// to send configuration.
+	c.C() <- &dap.InitializedEvent{
+		Event: dap.Event{
+			Event: "initialized",
+		},
+	}
+
+	c.Go(d.launch)
 	return nil
 }
 
 func (d *debugAdapter) Attach(c Context, req *dap.AttachRequest, resp *dap.AttachResponse) error {
 	close(d.started)
+
+	c.Go(d.launch)
 	return nil
 }
 
@@ -124,19 +131,95 @@ func (d *debugAdapter) Continue(c Context, req *dap.ContinueRequest, resp *dap.C
 	return nil
 }
 
-func (d *debugAdapter) Solve(ctx Context, c gateway.Client, req gateway.SolveRequest) (*gateway.Result, error) {
-	res, err := build.Solve(ctx, c, req)
+func (d *debugAdapter) SetBreakpoints(c Context, req *dap.SetBreakpointsRequest, resp *dap.SetBreakpointsResponse) error {
+	return nil
+}
 
-	if d.stopOnResult {
-		paused := d.Pause(ctx)
-		select {
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		case <-paused:
-		}
+func (d *debugAdapter) ConfigurationDone(c Context, req *dap.ConfigurationDoneRequest, resp *dap.ConfigurationDoneResponse) error {
+	d.configuration <- struct{}{}
+	close(d.configuration)
+	return nil
+}
+
+func (d *debugAdapter) launch(c Context) {
+	// Send initialized event.
+	c.C() <- &dap.InitializedEvent{
+		Event: dap.Event{
+			Event: "initialized",
+		},
 	}
 
-	return res, err
+	// Wait for configuration.
+	select {
+	case <-c.Done():
+		return
+	case <-d.configuration:
+		// TODO: actual configuration
+	}
+
+	for {
+		select {
+		case <-c.Done():
+			return
+		case req := <-d.solveReqCh:
+			if req == nil {
+				return
+			}
+
+			c.Go(func(c Context) {
+				defer close(req.resCh)
+				defer close(req.errCh)
+
+				res, err := d.solve(c, req.c, req.req)
+				if err != nil {
+					req.errCh <- err
+					return
+				}
+				req.resCh <- res
+			})
+		}
+	}
+}
+
+type solveRequest struct {
+	c     gateway.Client
+	req   gateway.SolveRequest
+	resCh chan<- *gateway.Result
+	errCh chan<- error
+}
+
+func (d *debugAdapter) solve(ctx Context, c gateway.Client, req gateway.SolveRequest) (*gateway.Result, error) {
+	// TODO: do debug things here such as catching errors.
+	return build.Solve(ctx, c, req)
+}
+
+func (d *debugAdapter) Solve(ctx context.Context, c gateway.Client, req gateway.SolveRequest) (*gateway.Result, error) {
+	resCh := make(chan *gateway.Result)
+	errCh := make(chan error)
+
+	// Send a solve request to the launch routine
+	// which will perform the solve in the context of the server.
+	sreq := &solveRequest{
+		c:     c,
+		req:   req,
+		resCh: resCh,
+		errCh: errCh,
+	}
+	select {
+	case d.solveReqCh <- sreq:
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+
+	// Wait for the response.
+	select {
+	case res := <-resCh:
+		return res, nil
+	case err := <-errCh:
+		return nil, err
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
 }
 
 func (d *debugAdapter) Threads(c Context, req *dap.ThreadsRequest, resp *dap.ThreadsResponse) error {
@@ -172,10 +255,12 @@ func (d *debugAdapter) Pause(c Context) <-chan struct{} {
 
 func (d *debugAdapter) Handler() Handler {
 	return Handler{
-		Initialize: d.Initialize,
-		Launch:     d.Launch,
-		Attach:     d.Attach,
-		Continue:   d.Continue,
-		Threads:    d.Threads,
+		Initialize:        d.Initialize,
+		Launch:            d.Launch,
+		Attach:            d.Attach,
+		Continue:          d.Continue,
+		SetBreakpoints:    d.SetBreakpoints,
+		ConfigurationDone: d.ConfigurationDone,
+		Threads:           d.Threads,
 	}
 }
