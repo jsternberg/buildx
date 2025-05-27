24adfeed0fb87288c87e9fdf3461096e4fde11b8
Jonathan A. Sternberg <jonathan@sternberg.sh>
2025-05-27 09:52:07 -0500

    add repl with a terminal interface

---
+++ build/debug/repl.go
@@ -0,0 +1,119 @@
+package debug
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"sort"
+	"text/tabwriter"
+
+	"github.com/docker/buildx/monitor/types"
+	"github.com/google/shlex"
+	"golang.org/x/term"
+)
+
+var additionalHelpMessages = map[string]string{
+	"help": "shows this message. Optionally pass a command name as an argument to print the detailed usage.",
+	"exit": "exits monitor",
+}
+
+type Terminal struct {
+	t                  *term.Terminal
+	registeredCommands map[string]types.Command
+}
+
+func NewTerminal(rdwr io.ReadWriter, prompt string) *Terminal {
+	t := term.NewTerminal(rdwr, prompt)
+	return &Terminal{t: t}
+}
+
+func (t *Terminal) Run(ctx context.Context, conn Conn) error {
+	for {
+		l, err := t.t.ReadLine()
+		if err != nil {
+			if err != io.EOF {
+				return err
+			}
+			return nil
+		}
+
+		args, err := shlex.Split(l)
+		if err != nil {
+			fmt.Fprintf(t.t, "monitor: failed to parse command: %v\n", err)
+			continue
+		} else if len(args) == 0 {
+			continue
+		}
+
+		// Builtin commands
+		switch args[0] {
+		case "":
+			// nop
+			continue
+		case "exit":
+			return nil
+		case "help":
+			if len(args) >= 2 {
+				t.printHelpMessageOfCommand(args[1])
+				continue
+			}
+			t.printHelpMessage()
+			continue
+		default:
+			t.invoke(ctx, args[0], args)
+		}
+	}
+}
+
+func (t *Terminal) invoke(ctx context.Context, cmd string, args []string) {
+	// Registered commands
+	if cm, ok := t.registeredCommands[cmd]; ok {
+		if err := cm.Exec(ctx, args); err != nil {
+			fmt.Fprintf(t.t, "%s: %v\n", cmd, err)
+		}
+	} else {
+		fmt.Fprintf(t.t, "monitor: unknown command: %q\n", cmd)
+		t.printHelpMessage()
+	}
+}
+
+func (t *Terminal) printHelpMessageOfCommand(name string) {
+	var target types.Command
+	if c, ok := t.registeredCommands[name]; ok {
+		target = c
+	} else {
+		fmt.Fprintf(t.t, "monitor: no help message for %q\n", name)
+		t.printHelpMessage()
+		return
+	}
+	fmt.Fprintln(t.t, target.Info().HelpMessage)
+	if h := target.Info().HelpMessageLong; h != "" {
+		fmt.Fprintln(t.t, h)
+	}
+}
+
+func (t *Terminal) printHelpMessage() {
+	var names []string
+	for name := range t.registeredCommands {
+		names = append(names, name)
+	}
+	for name := range additionalHelpMessages {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+	fmt.Fprint(t.t, "Available commands are:\n")
+	w := new(tabwriter.Writer)
+	w.Init(t.t, 0, 8, 0, '\t', 0)
+	for _, name := range names {
+		var mes string
+		if c, ok := t.registeredCommands[name]; ok {
+			mes = c.Info().HelpMessage
+		} else if m, ok := additionalHelpMessages[name]; ok {
+			mes = m
+		} else {
+			continue
+		}
+		fmt.Fprintln(w, "  "+name+"\t"+mes)
+	}
+	w.Flush()
+}
